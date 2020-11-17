b00dde98fef372f5f068e968da00ff8be6d76f18
localuser <achsanaris05@gmai.com>
2020-11-17 23:24:15 +0700

    Initial commit

---
+++ app/src/main/java/com/achsan_18102146/mylist/model/MyData.kt
@@ -0,0 +1,11 @@
+package com.achsan_18102146.mylist.model
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class MyData(
+    var name: String,
+    var description: String,
+    var photo: String
+) : Parcelable
